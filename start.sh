bbb519117ba9e753c45381611c3dc096245c8894
James Kirwa <example@email.com>
2019-08-09 22:11:52 +0300

    export mail & password in  start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export DATABASE_URL = 'postgresql+psycopg2://dnmrk:isystems123@localhost/blog'
 export MAIL_USERNAME='jameskirwa@gmail.com'
-export MAIL_PASSWORD='danmarkc'
+export MAIL_PASSWORD='Uppercase2000'
 
 python3.6 manage.py server
